1044bd3c0d4ad7b83c600e09d4f25329bec285a6
Ayush Khanal <aykh9013@colorado.edu>
2018-11-15 21:41:10 -0700

    some optimizations to program

--- three/three.hs
+++ three/three.hs
@@ -13,7 +13,12 @@ intsqrt::Integer -> Integer
 intsqrt x = floor (sqrt (fromIntegral x))
 
 isPrime::Integer -> Bool
-isPrime x = length([y | y <- takeWhile (<= intsqrt x) [2..], x `mod` y == 0 ]) == 0
+isPrime x
+    | x <= 1 = False
+    | x <= 3 = True
+    | x `mod` 2 == 0 = False
+    | x `mod` 3 == 0 = False 
+    | otherwise =  null [y | y <- [2..intsqrt x] ,x `mod` y == 0]
 
 anslist x = filter (isPrime) (factorizer x)
 ans x = head (anslist x)
